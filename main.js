c6456905d67f5493dbf2ffed205233e698175c93
Kevin Lacker <lacker@gmail.com>
2022-03-03 16:04:24 -0800

    fix bugs

--- main.js
+++ main.js
@@ -5,8 +5,8 @@ let width = window.innerWidth;
 let height = window.innerHeight;
 
 let scene = new THREE.Scene();
-let camera = new THREE.PerspectiveCamera(75, width / height, 1, 9);
-camera.position.z = 5;
+let camera = new THREE.PerspectiveCamera(75, width / height, 1, 19);
+camera.position.z = 10;
 
 // Lighting
 let color = 0xffffff;
@@ -20,12 +20,11 @@ let ambient = new THREE.AmbientLight(0x808080);
 scene.add(ambient);
 
 // Fresh Cut Day palette
-let background = 0xf9f2e7;
+let sun = 0xf9f2e7;
 let foreground = [0x00a8c6, 0x40c0cb, 0xaee239, 0x8fbe00];
 
 let renderer = new THREE.WebGLRenderer();
 renderer.setSize(width, height);
-// renderer.setClearColor(background);
 document.body.appendChild(renderer.domElement);
 
 function uniform(low, high) {
@@ -37,17 +36,18 @@ function randomizeBall(ball) {
   ball.position.x = uniform(-startRange, startRange);
   ball.position.y = uniform(-startRange, startRange);
   ball.position.z = uniform(-startRange, startRange);
-  ball.dx = uniform(-startRange, startRange);
-  ball.dy = uniform(-startRange, startRange);
-  ball.dz = uniform(-startRange, startRange);
-  ball.mass = 1;
-  ball.crash = false;
+  ball.dx = 2 * uniform(-startRange, startRange);
+  ball.dy = 2 * uniform(-startRange, startRange);
+  ball.dz = 2 * uniform(-startRange, startRange);
+  ball.crashed = false;
+  ball.crashPriority = Math.random();
 }
 
 let balls = [];
-let numBalls = 12;
+let ballRadius = 0.05;
+let numBalls = 500;
 for (let i = 0; i < numBalls; ++i) {
-  let geometry = new THREE.SphereGeometry(0.1, 32, 16);
+  let geometry = new THREE.SphereGeometry(ballRadius, 32, 16);
   let material = new THREE.MeshLambertMaterial({
     color: foreground[i % foreground.length]
   });
@@ -57,9 +57,15 @@ for (let i = 0; i < numBalls; ++i) {
   balls.push(ball);
 }
 
-let speed = 0.003;
-let gravity = 0.01;
-let crashDistance = 0.1;
+let sunRadius = 0.2;
+let geometry = new THREE.SphereGeometry(sunRadius, 32, 16);
+let material = new THREE.MeshLambertMaterial({ color: sun });
+let sunBall = new THREE.Mesh(geometry, material);
+scene.add(sunBall);
+
+let speed = 0.01;
+let ballToBallGravity = 0.00001;
+let sunToBallGravity = 0.5;
 
 function sq(x) {
   return x * x;
@@ -87,6 +93,7 @@ function animate() {
   }
 
   // Use gravity to update velocities
+  let newBalls = [];
   for (let ball of balls) {
     for (let other of balls) {
       let distance = Math.sqrt(
@@ -94,20 +101,43 @@ function animate() {
           sq(ball.position.y - other.position.y) +
           sq(ball.position.z - other.position.z)
       );
-      if (distance < crashDistance && distance > 0.001) {
-        randomizeBall(ball);
+      if (distance < ballRadius) {
+        if (distance > 0.00001 && ball.crashPriority < other.crashPriority) {
+          ball.crashed = true;
+        }
         continue;
       }
 
       // Gravity is proportional to inverse squared distance, and the difference between
       // positions is exactly the distance, so to get gravity we need to divide by cube distance
-      let multiplier =
-        (gravity * distance * distance * distance * other.mass) / ball.mass;
+      let multiplier = ballToBallGravity / (distance * distance * distance);
       ball.dx += multiplier * (other.position.x - ball.position.x);
       ball.dy += multiplier * (other.position.y - ball.position.y);
       ball.dz += multiplier * (other.position.z - ball.position.z);
     }
+
+    // Gravity toward the origin
+    let distance = Math.sqrt(
+      sq(ball.position.x) + sq(ball.position.y) + sq(ball.position.z)
+    );
+    if (distance < sunRadius + ballRadius) {
+      ball.crashed = true;
+    }
+    if (distance > 0.1) {
+      let multiplier = sunToBallGravity / (distance * distance * distance);
+      ball.dx -= multiplier * ball.position.x;
+      ball.dy -= multiplier * ball.position.y;
+      ball.dz -= multiplier * ball.position.z;
+    }
+
+    if (ball.crashed) {
+      scene.remove(ball);
+    } else {
+      newBalls.push(ball);
+    }
   }
+
+  balls = newBalls;
 
   renderer.render(scene, camera);
 }
